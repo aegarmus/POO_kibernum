770218b0c649746c8f0848821358c43daaa10aae
Alan Garcia <aegarmus@gmail.com>
2026-02-25 11:07:22 -0300

    feat: abstraccion y encapsulamiento

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,12 +20,88 @@
 
 */
 
+const REGEX_NAME = /^[a-zA-ZáéíóúÁÉÍÓÚ\s]+$/
+const REGEX_BIRTHDATE = /^\d{4}-\d{2}-\d{2}$/
 
 class Persona {
+    //Crear las propiedades privadas
+    #id
+    #name;
+    #lastName;
+    #birthDate;
+    #password
+
     constructor(name, lastName, birthDate) { // Función constructura
-        this.name = name;
-        this.lastName = lastName;
-        this.birthDate = birthDate;
+        this.#id = crypto.randomUUID()
+        this.#name = name;
+        this.#lastName = lastName;
+        this.#birthDate = birthDate;
+        this.#password = crypto.randomUUID()
+    }
+
+    // métodos privados -> Estos métodos no pueden ser accedidos por ninguna parte solo dentro del propio
+
+    #normalizeNames(name) {
+        /* const text = name.toLowerCase()
+        const nameSplited = text.split(' ')
+        const nameCapitalize = nameSplited.map(word => {
+            return(word.charAt(0).toUpperCase() + word.slice(1))
+        })
+        console.log(nameCapitalize.join(' ')) */
+        return name
+            .toLowerCase()
+            .split(' ') //Array con dos string
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ')
+    }
+
+    /** 
+     * Valida un regex dinámico y lanza un error en caso de no pasar la expresión regular
+     * @param {string} name - String a validar
+     * @param {regex} regex - Expresión regular validadora
+     * @param {string} message - campo opcional, mensaje personalizado para la excepción
+     * @throws {Error} - Lanza un mensaje predefinido si no se peorsonaliza el mensaje
+     * */ 
+    #validateRegex(name, regex, message) {
+        if (!regex.test(name)) {
+            throw new Error(message || 'Expresion invalida para este campo')
+        }
+    }
+
+    // Getters y Setters
+    // Getters son para obtener datos, y los Setters para modificar datos
+
+    get id() {
+        return this.#id;
+    }
+
+    get name() {
+        return this.#name
+    }
+
+    setName(newName) {
+        // Expresión Regular
+        this.#validateRegex(newName, REGEX_NAME, 'Nombre inválido')
+        this.#name = this.#normalizeNames(newName)
+        console.log('Nombre modificado con éxito')
+    }
+
+    get lasName() {
+        return this.#lastName
+    }
+
+    setLastName(newLastName) {
+        this.#validateRegex(newLastName, REGEX_NAME, 'Apellido inválido')
+        this.#lastName = this.#normalizeNames(newLastName)
+    }
+
+    get birthDate() {
+        return this.#birthDate
+    }
+
+    setBirthDate(newBirthDate) {
+        this.#validateRegex(newBirthDate, REGEX_BIRTHDATE, 'fecha de nacimiento invalida, usa el formato YYYY-MM-DD')
+        this.#birthDate = newBirthDate
     }
 
     /**
@@ -33,7 +109,7 @@ class Persona {
      * @returns {string} - retorna string con nombre completo
      */
     fullName() {
-        return `${this.name} ${this.lastName}`
+        return `${this.#name} ${this.#lastName}`
     }
 
     //JSdocs
@@ -43,7 +119,7 @@ class Persona {
      */
     getAge() {
         const today = new Date()
-        const birth = new Date(this.birthDate)
+        const birth = new Date(this.#birthDate)
         let age = today.getFullYear() - birth.getFullYear()
 
         const month = today.getMonth() - birth.getMonth()
@@ -53,11 +129,43 @@ class Persona {
 
         return age
     }
+
+    toObject() {
+        const objetoNuevo = {
+            id: this.#id,
+            name: this.#name,
+            lastName: this.#lastName,
+            birthDate: this.#birthDate,
+            password: this.#password
+        };
+
+        return objetoNuevo
+    }
+
+    toPublicObject() {
+        return {
+            id: this.#id,
+            name: this.#name,
+            lastName: this.#lastName,
+            birthDate: this.#birthDate,
+        };
+    }
 }
 
 const persona1 = new Persona('Juanito', 'Adasme', '1993-09-24')
 const persona2 = new Persona('Alan', 'García', '1993-09-25')
 
-console.log(persona1.fullName())
+
+console.log(persona1)
+console.log(persona1.toPublicObject())
+
+const personaCopy = persona1.toObject()
+/* 
+personaCopy.setName('Carlos') */
+
+persona1.setName('juan')
+console.log(persona1.name)
 console.log(persona2.getAge())
 
+persona1.setBirthDate('1994-02-20')
+console.log(persona1.birthDate)
